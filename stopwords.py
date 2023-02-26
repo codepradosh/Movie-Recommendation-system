57ea2f716b2bbaecc621bd9978918a8d47d6dbbe
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-02-26 15:13:17 +0530

    Update stopwords.py

--- stopwords.py
+++ stopwords.py
@@ -1,1 +1,2 @@
 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
